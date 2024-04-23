e39b92ef57e9e2ff0405c9babbdacdfa6c8e3b71
Paul <paul.micluta@ROMOB41178.amdaris.net>
2024-04-23 09:34:08 +0300

    Removed the unused non async methods from BaseRepository and IBaseRepository

--- VisualVibesBE/VisualVibes/VisualVibes.App/Interfaces/IBaseRepository.cs
+++ VisualVibesBE/VisualVibes/VisualVibes.App/Interfaces/IBaseRepository.cs
@@ -4,11 +4,6 @@ namespace VisualVibes.App.Interfaces
 {
     public interface IBaseRepository<T> where T : BaseEntity
     {
-        T Add(T entity);
-        T Update(T entity);
-        void Remove(T entity);
-        ICollection<T> GetAll();
-        T GetById(Guid id);
         Task<T> GetByIdAsync(Guid id);
         Task<T> AddAsync(T entity);
         Task<T> UpdateAsync(T entity);
--- VisualVibesBE/VisualVibes/VisualVibes.Infrastructure/Repositories/BaseRepository.cs
+++ VisualVibesBE/VisualVibes/VisualVibes.Infrastructure/Repositories/BaseRepository.cs
@@ -13,51 +13,33 @@ namespace VisualVibes.Infrastructure.Repositories
         {
             _logger = logger;
         }
-        public T Add(T entity)
-        {
-            if (_entities.Contains(entity))
-            {
-                Console.WriteLine($"Could not add the {nameof(T)}, because it already exists.");
-            }
-            _entities.Add(entity);
-            return entity;
-        }
+       
         public async Task<T> AddAsync(T entity)
         {
             if (_entities.Contains(entity))
             {
                 Console.WriteLine($"Could not add the {nameof(T)}, because it already exists.");
-                await _logger.LogAsync(nameof(Add), isSuccess: false);
+                await _logger.LogAsync(nameof(AddAsync), isSuccess: false);
                 return null;
             }
             _entities.Add(entity);
-            await _logger.LogAsync(nameof(Add), isSuccess: true);
+            await _logger.LogAsync(nameof(AddAsync), isSuccess: true);
             return entity;
-        }
-
-        public ICollection<T> GetAll()
-        {
-            return _entities;
         }
 
         public async Task<ICollection<T>> GetAllAsync()
         {
             if (_entities.Count == 0)
             {
-                await _logger.LogAsync(nameof(GetAll), isSuccess: false);
+                await _logger.LogAsync(nameof(GetAllAsync), isSuccess: false);
             }
             else
             {
-                await _logger.LogAsync(nameof(GetAll), isSuccess: true);
+                await _logger.LogAsync(nameof(GetAllAsync), isSuccess: true);
             }
             return _entities;
         }
 
-        public T GetById(Guid id)
-        {
-            var user = _entities.FirstOrDefault(e => e.Id == id);
-            return user;
-        }
         public async Task<T> GetByIdAsync(Guid id)
         {
             var user = _entities.FirstOrDefault(e => e.Id == id);
@@ -72,48 +54,31 @@ namespace VisualVibes.Infrastructure.Repositories
             return user;
         }
 
-
-        public void Remove(T entity)
-        {
-            if (!_entities.Contains(entity))
-            {
-                Console.WriteLine("The entity does not exist, therefore it could not be removed.");
-                return;
-            }
-            _entities.Remove(entity);
-        }
         public async Task<T> RemoveAsync(T entity)
         {
             if (!_entities.Contains(entity))
             {
                 Console.WriteLine("The entity does not exist, therefore it could not be removed.");
-                await _logger.LogAsync(nameof(Remove), isSuccess: false);
+                await _logger.LogAsync(nameof(RemoveAsync), isSuccess: false);
                 return null;
             }
             _entities.Remove(entity);
-            await _logger.LogAsync(nameof(Remove), isSuccess: true);
+            await _logger.LogAsync(nameof(RemoveAsync), isSuccess: true);
             return entity;
         }
 
-        public T Update(T updatedEntity)
-        {
-            var entity = _entities.FirstOrDefault(e => e.Id == updatedEntity.Id);
-            int index = _entities.IndexOf(entity);
-            _entities[index] = updatedEntity;
-            return updatedEntity;
-        }
         public async Task<T> UpdateAsync(T updatedEntity)
         {
             var entity = _entities.FirstOrDefault(e => e.Id == updatedEntity.Id);
             var index = _entities.IndexOf(entity);
             if (entity == null)
             {
-                await _logger.LogAsync(nameof(Update), isSuccess: false);
+                await _logger.LogAsync(nameof(UpdateAsync), isSuccess: false);
             }
             else
             {
                 _entities[index] = updatedEntity;
-                await _logger.LogAsync(nameof(Update), isSuccess: true);
+                await _logger.LogAsync(nameof(UpdateAsync), isSuccess: true);
             }
             return _entities[index];
         }
